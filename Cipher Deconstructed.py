670d2e80d896a294441382fc643330050149e288
deebaby001 <deemarie.hardy@gmail.com>
2024-01-28 21:26:16 -0500

    Updates to add comments to 2 files

--- Cipher Deconstructed.py
+++ Cipher Deconstructed.py
@@ -1,16 +1,16 @@
 import string
-def caesar(text, shift, alphabets):
+def caesar(text, shift, alphabets):            #Defining caesar
     
-    def shift_alphabet(alphabet):
+    def shift_alphabet(alphabet):                 #Defining the shifted alphabet
         return alphabet[shift:] + alphabet[:shift]
     
-    begin_shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    begin_shifted_alphabets = tuple(map(shift_alphabet, alphabets))       #Use tuple to map the definitions then use joins
     end_alphabet = ''.join(alphabets)
     end_shifted_alphabet = ''.join(begin_shifted_alphabets)
-    table = str.maketrans(end_alphabet, end_shifted_alphabet)
+    table = str.maketrans(end_alphabet, end_shifted_alphabet)  #indicating the translation table
     return text.translate(table)
 
-plain_text = "Ymnx nx Ijj Yjxy= Mjqqt Btwqi&"
+plain_text = "Ymnx nx Ijj Yjxy= Mjqqt Btwqi&" #Converting or deciphering the message to the original string; performing shift
 shift = 26 - 5
 shift %= 26
 print(caesar(plain_text, 21, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
--- New Cipher Encrypt.py
+++ New Cipher Encrypt.py
@@ -1,14 +1,14 @@
 import string
 
-def caesar(text, shift, alphabets):
+def caesar(text, shift, alphabets):                #Defining caesar
     
-    def shift_alphabet(alphabet):
+    def shift_alphabet(alphabet):                       #Defining shifted alphabet 
         return alphabet[shift:] + alphabet[:shift]
     
-    begin_shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    begin_shifted_alphabets = tuple(map(shift_alphabet, alphabets))             #Use tuple to map definitions then use joins
     end_alphabet = ''.join(alphabets)
     end_shifted_alphabet = ''.join(begin_shifted_alphabets)
-    table = str.maketrans(end_alphabet, end_shifted_alphabet)
+    table = str.maketrans(end_alphabet, end_shifted_alphabet)                      #Indicating the translation table
     return text.translate(table)
 plain_text = "This is Dee Test. Hello World!"
 print(caesar(plain_text, 5, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
